e0135d30deadc18179b927591878c5b211908d32
Queen-Liesse <kayitare.03.11@gmail.com>
2022-08-04 16:00:13 +0200

    add charge

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
-function send() {
-  let amount = parseInt(document.getElementById("money").value);
-  let money = 1000;
-  let answer;
-  let charge;
-  // document.getElementById("total").innerHTML = answer;
-  // document.getElementById("sends").innerHTML = amount;
-  if (amount > money) {
-    alert("Nta mafaranga ufitemo ahagije yo gusoza icyo gikorwa");
-  } else if (amount >= 1 && amount < 10000) {
-    charge = 0;
-    answer = money - amount - charge;
-    document.getElementById("total").innerHTML = answer;
-    document.getElementById("initial").innerHTML = answer;
-    document.getElementById("charge").innerHTML = charge;
-  } else if (amount >= 10000 && amount < 100000) {
-    charge = 200;
-    answer = money - amount - charge;
-    document.getElementById("total").innerHTML = answer;
-    document.getElementById("initial").innerHTML = answer;
-    document.getElementById("charge").innerHTML = charge;
-  } else if (amount >= 100000) {
-    charge = 1000;
-    answer = money - amount - charge;
-    document.getElementById("total").innerHTML = answer;
-    document.getElementById("initial").innerHTML = answer;
-    document.getElementById("charge").innerHTML = charge;
+let balance = document.getElementById("initial");
+let receive = document.getElementById("receive");
+let send = document.getElementById("send");
+let receiveInput = document.getElementById("receive-input");
+let sendInput = document.getElementById("send-input");
+let receivedBtn = document.getElementById("btn-receive");
+let sendBtn = document.getElementById("btn-send");
+let charge = document.getElementById("charge");
+
+receivedBtn.addEventListener("click", () => {
+  const value = receiveInput.value;
+  const receiveValue = Number(receive.innerText) + Number(value);
+  const balanceValue = Number(balance.innerText) + Number(value);
+  receive.innerText = receiveValue;
+  balance.innerText = balanceValue;
+});
+sendBtn.addEventListener("click", () => {
+  const value = sendInput.value;
+  if (Number(value) === 0) {
+    alert("you don't have any money to send");
+  } else if (Number(value) > Number(balance.innerText)) {
+    alert("ntabwo ufite amafaranga agahije yo gusoza icyo gikorwa");
+  } else if (Number(value) >= 10000 && Number(value <= 100000)) {
+    const chargeValue = 200;
+    const sendValue = Number(send.innerText) + Number(value);
+
+    const balanceValue =
+      Number(balance.innerText) - Number(value) - chargeValue;
+    send.innerText = sendValue;
+    charge.innerText = chargeValue;
+    balance.innerText = balanceValue;
+  } else if (Number(value) >= 100000) {
+    const chargeValue = 1000;
+    const sendValue = Number(send.innerText) + Number(value);
+
+    const balanceValue =
+      Number(balance.innerText) - Number(value) - chargeValue;
+    send.innerText = sendValue;
+    charge.innerText = chargeValue;
+    balance.innerText = balanceValue;
+  } else {
+    const sendValue = Number(send.innerText) + Number(value);
+
+    const balanceValue = Number(balance.innerText) - Number(value);
+    send.innerText = sendValue;
+    balance.innerText = balanceValue;
   }
-}
-
-function receive() {
-  let amount = parseInt(document.getElementById("money").value);
-  let money = 1000;
-  let answer = money + amount;
-
-  document.getElementById("total").innerHTML = answer;
-  document.getElementById("receive").innerHTML = amount;
-}
+});
 
 $(".message a").click(function () {
   $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
